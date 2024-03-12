6c76f3dbc3794454f1488f23b8befe2fbe055976
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-03-12 15:34:26 +0100

    Add FAQ page

---
+++ src/components/FAQ/Question.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Question({ question, answer }: { question: string; answer: string }) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const questionContainerStyles = {
+    padding: "1rem",
+    width: "60%",
+    fontSize: "1.8rem",
+    gap: "1rem",
+    border: "1px solid gray",
+    cursor: "pointer",
+  };
+
+  const answerStyles = {
+    marginTop: "1rem",
+  };
+
+  const flexDivStyles = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
+
+  return (
+    <div onClick={handleClick} style={questionContainerStyles}>
+      <div style={flexDivStyles}>
+        <p>{question}</p>
+        {isClicked ? (
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/minus.png" />
+        ) : (
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/plus.png" />
+        )}
+      </div>
+      <div style={{ display: isClicked ? "block" : "none" }}>
+        <hr />
+        <p style={answerStyles}>{answer}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
